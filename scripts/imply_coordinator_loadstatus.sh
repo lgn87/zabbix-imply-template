17127f2422340d689e34eb4d67181db1dc266475
Dusan Umljenovic <dusan.umljenovic@bravosystems.com>
2020-04-25 15:08:34 +0200

    init

---
+++ scripts/imply_coordinator_loadstatus.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USER="username"
+PASSWORD="password"
+
+
+json=$(curl -s -u $USER:$PASSWORD http://$1:8081/druid/coordinator/v1/loadstatus?simple)
+value=$(echo $json | jq . | grep -v { | grep -v } | cut -f2 -d":" | cut -f1 -d"," | grep -v 0)
+
+if [ -z $value ];
+then
+   echo 0
+else
+   echo 1
+fi
